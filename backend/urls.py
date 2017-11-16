54fdc1fd253bb6110e2b49dc5f8885560366fa5d
youxingzhi <youxingzhi@qq.com>
2017-11-16 17:07:06 +0800

    使用 routers register

--- backend/urls.py
+++ backend/urls.py
@@ -15,15 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.views.static import serve
+from rest_framework.routers import DefaultRouter
 from rest_framework.documentation import include_docs_urls
 
 import xadmin
 from settings import MEDIA_ROOT
 from goods.views import GoodsListViewSet
 
-goods_list = GoodsListViewSet.as_view({
-    'get': 'list'
-})
+router = DefaultRouter()
+
+# 配置goods的url
+router.register(r'goods', GoodsListViewSet)
+
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
@@ -34,7 +37,7 @@ urlpatterns = [
     # url(r'goods/$', GoodsListView.as_view(), name="goods_list"),
 
     # 商品列表页 method 2
-    url(r'goods/$', goods_list, name="goods_list"),
+    url(r'^', include(router.urls)),
 
     url(r'docs/', include_docs_urls(title="游先生的生鲜")),
 ]
