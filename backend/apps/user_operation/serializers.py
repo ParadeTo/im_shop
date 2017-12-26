54114a73964f56ec9d055f2f37c393900ae60567
youxingzhi <youxingzhi@qq.com>
2017-12-26 15:37:19 +0800

    用户收藏

--- backend/apps/user_operation/serializers.py
+++ backend/apps/user_operation/serializers.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers
-
+from rest_framework.validators import UniqueTogetherValidator
 from user_operation.models import UserFav
 
 
 class UserFavSerializer(serializers.ModelSerializer):
+    # 当前用户
     user = serializers.HiddenField(
         default=serializers.CurrentUserDefault()
     )
+
     class Meta:
         model = UserFav
-        fields = ('user', 'goods')
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset=UserFav.objects.all(),
+                fields=('user', 'goods'),
+                message="已收藏"
+            )
+        ]
+
+        fields = ('user', 'goods', "id")
--- backend/apps/user_operation/views.py
+++ backend/apps/user_operation/views.py
@@ -2,11 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from user_operation.models import UserFav
+from user_operation.permissions import IsOwnerOrReadOnly
 from user_operation.serializers import UserFavSerializer
 
 
-class UserFavViewset(mixins.CreateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
-    queryset = UserFav.objects.all()
+class UserFavViewset(mixins.CreateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.ListModelMixin,
+                     mixins.DestroyModelMixin,
+                     viewsets.GenericViewSet):
+    """
+    用户收藏
+    """
     serializer_class = UserFavSerializer
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly) # 只能删除自己的收藏
+    authentication_classes = (JSONWebTokenAuthentication, )
+
+    def get_queryset(self):
+        return UserFav.objects.filter(user=self.request.user)
